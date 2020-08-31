d06083e5037273c929ed948481e4a18ebd040e20
Benjamin Gao <bgao01@mit.edu>
2020-08-31 17:59:56 -0400

    Added files

---
+++ Assets/Standard Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+    private Vector3 offset;
+     
+    /* Use this for initialization. This calculates the difference between
+     * the camera's position and the player. */
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
+	}
+	
+	/* Update is called once per frame. This always allows the player to follow
+     * the player. The camera will be a set distance from the player.*/
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Standard Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    /*Every fixed interval of time, this information is updated.
+     * This code implements forces on the player object. */
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    /*When the player object hits one of the pick ups, the pick up is deactivated.
+     * The count for number of pick ups increases by one, and the UI displays this change.*/
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    //This method displays the current number of pick ups collected. 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
